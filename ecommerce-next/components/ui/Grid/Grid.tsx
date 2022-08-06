ef6f16e7cbf321161b2096615b5793a63862f36f
tsuki1646 <aoanhtrang_16@yahoo.com>
2022-08-07 01:43:27 +0900

    Less93: Layout A Col

--- ecommerce-next/components/ui/Grid/Grid.tsx
+++ ecommerce-next/components/ui/Grid/Grid.tsx
@@ -1,13 +1,19 @@
 import { FC, ReactNode } from "react";
 import s from "./Grid.module.css";
+import cn from "classnames";
 
 interface Props {
   children: ReactNode[];
   layout?: "A" | "B";
 }
 
-const Grid: FC<ReactNode> = ({ children }) => {
-  return <div className={s.root}>{children}</div>;
+const Grid: FC<Props> = ({ children, layout = "A" }) => {
+  const rootClassName = cn(s.root, {
+    [s.layoutA]: layout === "A",
+    [s.layoutB]: layout === "B",
+  });
+
+  return <div className={rootClassName}>{children}</div>;
 };
 
 export default Grid;
