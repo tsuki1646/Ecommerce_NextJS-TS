6ddf907011976dea77cb06496911c1ec853fcb44
tsuki1646 <aoanhtrang_16@yahoo.com>
2022-08-10 00:42:52 +0900

    Less114: Ticker

--- ecommerce-next/components/ui/Container/Container.tsx
+++ ecommerce-next/components/ui/Container/Container.tsx
@@ -1,15 +1,12 @@
-import { ReactNode, FC } from "react";
+import { ReactNode, FC, ComponentType, HTMLAttributes } from "react";
 
 interface Props {
   children: ReactNode | ReactNode[];
+  el?: ComponentType<HTMLAttributes<HTMLElement>>;
 }
 
-const Container: FC<Props> = ({ children }) => {
-  return (
-    <div style={{ maxWidth: "1920px" }} className="px-6 mx-auto">
-      {children}
-    </div>
-  );
+const Container: FC<Props> = ({ children, el: Component = "div" }) => {
+  return <Component className="px-6 mx-auto max-w-8xl">{children}</Component>;
 };
 
 export default Container;
---
+++ ecommerce-next/components/ui/Marquee/Marquee.tsx
@@ -0,0 +1,38 @@
+import { ReactNode, FC } from "react";
+import ReactFastMarquee from "react-fast-marquee";
+import s from "./Marquee.module.css";
+import cn from "classnames";
+
+interface Props {
+  children: ReactNode[];
+  direction?: "left" | "right";
+  variant?: "primary" | "secondary";
+  gradient?: boolean;
+}
+
+const Marquee: FC<Props> = ({
+  children,
+  direction = "left",
+  variant = "primary",
+  gradient,
+}) => {
+  const rootClassName = cn(s.root, {
+    [s.secondary]: variant === "secondary",
+  });
+  return (
+    <div className={s.root}>
+      {/* and this  */}
+      <ReactFastMarquee
+        speed={50}
+        gradient={gradient}
+        direction={direction}
+        loop={0}
+        play={true}
+      >
+        <div className={s.container}>{children}</div>
+      </ReactFastMarquee>
+    </div>
+  );
+};
+
+export default Marquee;
