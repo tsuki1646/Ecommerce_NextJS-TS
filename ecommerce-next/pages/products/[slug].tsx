7dcf75f151f896d3680ed8f92792e9a40a7e54b1
tsuki1646 <aoanhtrang_16@yahoo.com>
2022-08-12 19:29:21 +0900

    Less136: Pass variables to query

--- ecommerce-next/pages/products/[slug].tsx
+++ ecommerce-next/pages/products/[slug].tsx
@@ -27,7 +27,10 @@ export const getStaticProps = async ({
   params,
 }: GetStaticPropsContext<{ slug: string }>) => {
   const config = getConfig();
-  const { product } = await getProduct(config);
+  const { product } = await getProduct({
+    config,
+    variables: { slug: params?.slug },
+  });
   return {
     props: {
       product,
